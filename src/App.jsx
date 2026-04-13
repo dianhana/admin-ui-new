1e8c00c950e461a2ee563fb3bddf779b62094625
dianhana <dianhanakartikosari@gmail.com>
2026-04-13 20:09:54 +0700

    fix: sync project clean

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import "./App.css";
+
+function App() {
+  const courses = [
+    { title: "System Administration and IT Infrastructure Services" },
+    { title: "Operating Systems Becoming a Power User" },
+    { title: "The Bits and Bytes of Computer Networking" },
+    { title: "Technical Support Fundamentals" },
+    { title: "How to Succeed at: Writing Applications" },
+    { title: "Medicine Administration for Carers" },
+  ];
+
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <div className="max-w-7xl mx-auto p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          
+          {courses.map((course, index) => (
+            <div
+              key={index}
+              className="shadow rounded-lg overflow-hidden flex flex-col bg-white
+              border border-gray-300 hover:border-blue-500
+              hover:shadow-lg hover:scale-105 transition duration-300"
+            >
+              {/* IMAGE */}
+              <img
+                src="https://placehold.co/300x200"
+                alt={course.title}
+                className="w-full h-44 object-cover hover:scale-105 transition"
+              />
+
+              {/* CONTENT */}
+              <div className="p-4 bg-red-100 flex flex-col justify-between h-full">
+                <h3 className="text-lg font-semibold mb-4">
+                  {course.title}
+                </h3>
+
+                <div className="bg-red-50 p-2 rounded-lg">
+                  <div className="text-sm text-gray-500 mt-2 flex justify-between">
+                    <span>👥 123 users</span>
+                    <span>⏱ 60 min</span>
+                  </div>
+
+                  {/* Author */}
+                  <div className="mt-3 mb-2 flex items-center gap-2">
+                    <img
+                      src="https://placehold.co/50x50"
+                      alt="Author"
+                      className="w-12 h-12 rounded-full object-cover"
+                    />
+                    <div>
+                      <p className="text-sm font-medium">Author's Name</p>
+                      <p className="text-xs text-gray-500">Designer</p>
+                    </div>
+                  </div>
+                </div>
+
+                {/* FOOTER */}
+                <div className="mt-4 flex items-center justify-between">
+                  <div className="bg-white py-2 px-4 rounded">$123</div>
+                  <button className="bg-red-700 hover:bg-red-900 active:bg-red-500 text-white px-4 py-2 rounded text-sm">
+                    Get Started
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
