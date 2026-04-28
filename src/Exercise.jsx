5cbf0ff0269a1f562394f5b3307609bcc00a8693
dianhana <dianhanakartikosari@gmail.com>
2026-04-28 08:42:16 +0700

    UTS

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,33 +1,20 @@
 import React from "react";
 import UserCard from "./UserCard";
+import users from "./data";
 
 function Exercise() {
-    
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
-        </h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard name="Dian"
-          email="dian@gmail.com"
-          street="Jl.Sudirman"
-          city="Semarang"
-          />
-          <UserCard name="Hana"
-          email="hana@gmail.com"
-          street="Jl.poncowolo"
-          city="Pati"
-          />
-          <UserCard name="Sari"
-          email="sari@gmail.com"
-          street="Jl.pandanaran"
-          city="Semarang"
-          />
-        </div>
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6 text-[#e73d1c]">
+        Post Card
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 max-w-7xl mx-auto">
+        {users.map((user, index) => (
+          <UserCard key={index} {...user} />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,30 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function UserCard(props){
-    const{name, email,street, city} = props;
-    const [clicked, setClicked] = useState(false);
+function UserCard({ id, userId, title, body }) {
+  const [clicked, setClicked] = useState(false);
 
-    // Fungsi handler
-    function handleClick() {
-        setClicked(true);
-    }
+  return (
+    <div className="bg-white p-6 rounded-lg shadow max-w-sm w-full mx-auto transition-all duration-300 
+                    hover:scale-105 hover:border hover:bg-red-100 
+                    flex flex-col">
+      
+      <h2 className="text-xl font-semibold text-gray-800 mb-2 text-center">
+        {title}
+      </h2>
 
-      return (
-    <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow">
-      <h2 className="text-xl font-semibold text-gray-800 mb-2">{name}</h2>
-      <p className="text-gray-600">
-        <span className="font-medium">Email:</span> {email}
+      <p className="text-gray-600 mb-4">
+        {body}
       </p>
-      <p className="text-gray-600">
-        <span className="font-medium">Address:</span> {street} {city}
-      </p>
-      <button className="bg-gray-01 text-white p-2 rounded-md"
-      className={`${clicked ? "bg-special-green" : "bg-gray-01"} text-white p-2 rounded-md`}
-      onClick={() => setClicked(true)}
+
+      <button
+        onClick={() => setClicked(true)}
+        className={`mt-auto p-2 rounded-md text-white transition
+          ${
+            clicked
+              ? "bg-special-red2 hover:bg-red-600"
+              : "bg-gray-500 hover:bg-gray-400"
+          }`}
       >
         {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
-        </button>
+      </button>
     </div>
   );
 }
-export default UserCard
+
+export default UserCard;
